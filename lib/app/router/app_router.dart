2dfa03323f23aac6b3bd82ee2a3ba391d57a56a9
BrahimAkar <brahim.akarouch@gmail.com>
2023-03-11 15:35:52 +0100

    Update router to remove HomeView import

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:miniflux_app/presentation/views/home_view.dart';
 import 'package:miniflux_app/presentation/views/user_feed/views/user_feed.dart';
 
 part 'app_router.gr.dart';
