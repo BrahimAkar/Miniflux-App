637491a857d95b8c3e0094f88235f200c9f97f22
BrahimAkar <brahim.akarouch@gmail.com>
2023-03-11 17:58:15 +0100

    Refactor getMainUrl function to exclude port from URL

--- lib/utils/functions/get_main_url_from_url.dart
+++ lib/utils/functions/get_main_url_from_url.dart
@@ -2,7 +2,7 @@ String getMainUrl(String url) {
   var uri = Uri.parse(url);
   var scheme = uri.scheme;
   var host = uri.host;
-  var port = uri.port;
+
   var mainUrl = '$scheme://$host';
 
   return mainUrl;
